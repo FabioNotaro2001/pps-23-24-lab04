e6fe7805d76cf606c7684ce6b73df2925f6cac3c
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-03-26 16:06:01 +0100

    Completed ex3

--- src/main/scala/tasks/adts/Ex3Stack.scala
+++ src/main/scala/tasks/adts/Ex3Stack.scala
@@ -1,7 +1,10 @@
 package tasks.adts
 
 import u03.Sequences.*
+import Sequence.*
 import u03.Optionals.*
+import u04.adts.SequenceADT.nil
+import u02.AlgebraicDataTypes.IntList
 
 /*  Exercise 3: 
  *  Implement a Stack ADT
@@ -22,9 +25,18 @@ object Ex3Stacks:
       def asSequence(): Sequence[A]
   
   object StackImpl extends StackADT:
-    type Stack[A] = Nothing
-    def empty[A]: Stack[A] = ???
+    type Stack[A] = Sequence[A]
+    def empty[A]: Stack[A] = Nil()
     extension [A](stack: Stack[A])
-      def push(a: A): Stack[A] = ???
-      def pop(a: A): Optional[(A, Stack[A])] = ???
-      def asSequence(): Sequence[A] = ???
+      def push(a: A): Stack[A] = stack match
+        case Nil() => Cons(a, Nil())
+        case Cons(h, t) => Cons(a, Cons(h, t))
+      
+      def pop(a: A): Optional[(A, Stack[A])] = stack match
+        case Nil() => Optional.Empty()
+        case Cons(h, t) => Optional.Just(h, t)
+      
+      def asSequence(): Sequence[A] = stack match
+        case Nil() => Nil()
+        case Cons(h, t) => Cons(h, t)
+      
